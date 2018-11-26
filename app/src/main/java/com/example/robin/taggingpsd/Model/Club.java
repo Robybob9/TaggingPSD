d0818770441c202a954d84c6dc504d8267adbcb4
monopoly <robingoesseye@hotmail.com>
2018-11-26 09:43:02 +0100

    klasses aanmaken

---
+++ app/src/main/java/com/example/robin/taggingpsd/Model/Club.java
@@ -0,0 +1,7 @@
+package app.src.main.java.com.example.robin.taggingpsd.Model;
+
+import java.util.List;
+
+public class Club{
+    private List<Ploeg> ploegen;
+}
--- app/src/main/java/com/example/robin/taggingpsd/Model/Speler.java
+++ app/src/main/java/com/example/robin/taggingpsd/Model/Speler.java
@@ -1,5 +1,7 @@
 package app.src.main.java.com.example.robin.taggingpsd.Model;
 
 public class Speler {
-
+    private String achterNaam;
+    private String voorNaam;
+    private Club club;
 }
